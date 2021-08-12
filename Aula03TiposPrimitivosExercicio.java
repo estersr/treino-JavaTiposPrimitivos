c3a58be48a58ddc751a8b151f6de20906b7ff9f1
estersr <87035589+estersr@users.noreply.github.com>
2021-08-12 20:55:54 -0300

    01. Java Basics - Practice
    Practicing.
    Praticando os tipos primitivos do Java.

---
+++ Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Kirito";
+        String endereco = "Av joao das flores";
+        double salario = 5815.35;
+        String dataRecebimento = "20/12/2021";
+        String relatorio = "Eu " + nome + ", morando no endereço " + endereco + "confirmo que recebi o salario de " + salario + ",na data " + dataRecebimento;
+
+
+        System.out.println(relatorio);
+
+
+    }
+}
